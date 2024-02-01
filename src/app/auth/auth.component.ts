5ce95c2a35cd8b78c667cd5a6e5b5f400a904282
roman <romanpz051@gmail.com>
2024-02-01 20:02:37 +0100

    recipe-cook v0.0.12_auth-login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
-import {AuthService} from "./auth.service";
+import {AuthResponseData, AuthService} from "./auth.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-auth',
@@ -24,21 +25,28 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
-    this.isLoading = true;
-    if (this.isLoginMode) {
+    let authObs: Observable<AuthResponseData>;
 
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe(resData => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        error => {
-          console.log(error);
-          this.error = 'An error occurred!';
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signUp(email, password);
     }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        console.log(resData.registered);
+        this.isLoading = false;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {catchError, throwError} from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: "root"})
@@ -18,6 +20,29 @@ export class AuthService {
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCDvz1n6Sd6ySioMENpChRwkJVINyegi1M',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(
+      catchError(errorRes => {
+        let errorMessage = 'An unknown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCDvz1n6Sd6ySioMENpChRwkJVINyegi1M',
       {
         email: email,
         password: password,
